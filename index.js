545960ab15a1f39919b361243213d2ab099a6720
cobansson <b.engincobanoglu@gmail.com>
2022-11-03 15:50:50 +0100

    initialize fourth commit
    fourth commit

--- index.js
+++ index.js
@@ -1,62 +1,110 @@
 import { menuArray } from "./data.js";
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
 
 const menuContainer = document.getElementById("menu-container");
+const orderContainer = document.getElementById("order-container");
+const totalPrice = document.getElementById("total-price");
+const listFood = document.getElementById("list-food");
 let yourOrderArray = [];
 
 
 document.addEventListener("click", function(e) {
     if(e.target.dataset.add) {
         handleOrderInput(e.target.dataset.add);
+    } else if(e.target.dataset.delete) {
+        handleDeleteButton(e.target.dataset.delete);
     }
 })
 
+function handleDeleteButton(targetId) {
+    const deletedOrder = yourOrderArray.filter((order) => {
+        return order.uuid === targetId;
+    })[0];
+    
+    for (let i = 0; i < yourOrderArray.length; i++) {
+        if (yourOrderArray[i] === deletedOrder) {
+            yourOrderArray.splice(i, 1);
+        }
+    }
+    render();
+}
+
 function handleOrderInput(foodId) {
-    const OrderedFoodObject = menuArray.filter(function(food) {
+
+    // Filter out the object to the const so that it will know which food you want to choose //
+
+    const OrderedFoodObject = menuArray.filter((food) => {
         return food.uuid === foodId;
     })[0];
 
-    yourOrderArray.push({
-       name: OrderedFoodObject.name,
-       price: OrderedFoodObject.price,
-    });
+    // Push the selected items in the array //
 
-    let yourOrderHtml = ``;
-    yourOrderArray.forEach(order => {
-        return yourOrderHtml +=
+    yourOrderArray.push({
+        name: OrderedFoodObject.name,
+        price: OrderedFoodObject.price,
+        uuid: uuidv4()
+    })
+
+    // This section renders the order list //
+
+    let orderList = "";
+
+    yourOrderArray.forEach((order) => {
+
+        orderList +=
         `
-        <div class="order-list" id="order-list">
-            <h6>${order.name}</h6>
-            <div class="price">
-                <button class="remove-btn">remove</button>
-                <h6>€${order.price}</h6>
+        <li class="" id="list-${order.uuid}">
+            <div class="order-list" id="order-list">
+                <h6>${order.name}</h6>
+                <div class="price">
+                    <button class="remove-btn" data-delete="${order.uuid}">remove</button>
+                    <h6>€${order.price}</h6>
+                </div>
             </div>
-        </div>
+        </li>
         `
     })
-    document.getElementById("order-container").classList.remove("hidden");
-    document.getElementById("order-list").innerHTML = yourOrderArray;
+
+    listFood.innerHTML = orderList;
+
+    // This section renders the total price //
+
+    let SumPrice = 0;
+
+    yourOrderArray.forEach((price) => {
+        SumPrice += price.price
+    })
+    totalPrice.textContent = "€" + SumPrice;
+
+    // This section unhidden the order list when clicked //
+
+    orderContainer.classList.remove("hidden");
     render();
 }
+
+// This function renders out menu list //
 
 function getFoodList() {
     let foodList = ``;
 
-    menuArray.forEach(food => {
+    menuArray.forEach((food) => {
         foodList +=
         `
         <div class="menu-list" id="menu-list">
             <div class="food-container" id="food-container">
-                <img src="${food.image}" alt="${food.alt}">
-                <div class="food-info">
-                    <div class="food-details">
-                        <h5 class="food-name">${food.name}</h5>
-                        <p>${food.ingredients}</p>
-                        <h6>€${food.price}</h6>
+                <div class="firstPart"> 
+                    <img src="${food.image}" alt="${food.alt}">
+                    <div class="food-info">
+                        <div class="food-details">
+                            <h5 class="food-name">${food.name}</h5>
+                            <p>${food.ingredients}</p>
+                            <h6>€${food.price}</h6>
+                        </div>
                     </div>
-                    <div>
-                        <button class="add-btn" data-add="${food.uuid}">+</button>
-                    </div>
+                </div>
+                <div>
+                    <button class="add-btn" data-add="${food.uuid}">+</button>
                 </div>
             </div>
         </div>
